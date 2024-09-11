544a855ca2d4103002145932087594ffd27341c5
Wahdaapf <wahdaadella.ba@gmail.com>
2024-09-10 18:06:55 -0700

    feat: kasir kafe

---
+++ Kafe25.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Kafe25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean keanggotaan;
+        int jmlKopi, jmlTeh, jmlRoti;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga;
+        float diskon = 10 / 100f;
+
+        System.out.print("Masukkan Keanggotaan (true/false)\t: ");
+        keanggotaan = sc.nextBoolean();
+        System.out.print("Masukkan Jumlah Pembelian Kopi\t\t: ");
+        jmlKopi = sc.nextInt();
+        System.out.print("Masukkan Jumlah Pembelian Teh\t\t: ");
+        jmlTeh = sc.nextInt();
+        System.out.print("Masukkan Jumlah Pembelian Roti\t\t: ");
+        jmlRoti = sc.nextInt();
+
+        totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+        totalHarga = totalHarga - (totalHarga * diskon);
+        totalHarga = Math.round(totalHarga);
+
+        System.out.printf("\nOutput:\n\nKeanggotaan Pelanggan\t: %s\nItem Pembelian\t\t: %s Kopi, %s Teh, %s Roti\nNominal Bayar Rp.\t: %s", keanggotaan, jmlKopi, jmlTeh, jmlRoti, totalHarga);
+    }
+}
